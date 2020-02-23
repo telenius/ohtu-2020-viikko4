f2885807f878e2c9f84731159c87409038e75289
Jelena Telenius <13237079+telenius@users.noreply.github.com>
2020-02-23 21:15:26 +0000

    viikko 4 teht4 valmis

--- viikko4_teht3to4_Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4_teht3to4_Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -24,7 +24,7 @@ public class KauppaTest {
     
         // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
         when(varasto.saldo(1)).thenReturn(10); 
-        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));          
 
         // määritellään että tuote numero 2 on kakku jonka hinta on 7 ja saldo 3
         when(varasto.saldo(2)).thenReturn(3); 
@@ -39,6 +39,7 @@ public class KauppaTest {
                 k = new Kauppa(varasto, pankki, viite);              
         }
 
+   
     @Test
     public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
 
@@ -109,6 +110,70 @@ public class KauppaTest {
         // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu oikeilla arvoilla        
         verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), eq("33333-44455"), eq(5));   
                 
+    }
+    
+    @Test
+    public void aloitaAsiointiNollaaKaupanTiedot() {
+        // oikealla asiakkaalla, tilinumeroilla ja summalla
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        
+        // keskeytetaan - ja alotetaan alusta
+        k.aloitaAsiointi();
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu nollatuilla arvoilla        
+        verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(), eq(0));   
+                
+    }
+    
+    @Test
+    public void uusiViitenumeroJokaMaksulle() {
+        // oikealla asiakkaalla, tilinumeroilla ja summalla
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+        
+        // tarkistetaan että tässä vaiheessa viitegeneraattorin metodia seuraava()
+        // on kutsuttu kerran
+        verify(viite, times(1)).uusi();
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+        
+        // tarkistetaan että tässä vaiheessa viitegeneraattorin metodia seuraava()
+        // on kutsuttu kaksi kertaa
+        verify(viite, times(2)).uusi();
+                
+    }
+
+    @Test
+    public void poistetaanTuoteKorista() {
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+               
+        // tarkistetaan että tässä vaiheessa varaston metodia palautaVarastoon()
+        // on kutsuttu kerran
+        Tuote testituote=varasto.haeTuote(1);
+        verify(varasto, times(0)).palautaVarastoon(testituote);
+
+        k.poistaKorista(1);     // palautetaan varastoon tuotetta numero 1 eli maitoa
+        
+        // tarkistetaan että tässä vaiheessa varaston metodia palautaVarastoon()
+        // on kutsuttu kerran
+        verify(varasto, times(1)).palautaVarastoon(testituote);
+
     }
 
     
